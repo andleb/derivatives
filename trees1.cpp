024e7f8022e71a10beb405ffcdf4a9970a69471f
Andrej Leban <andrejleban@gmail.com>
2019-07-13 13:54:11 +0200

    Ch. 8: Trees init

---
+++ trees1.cpp
@@ -0,0 +1,23 @@
+/** \file exotics1.cpp
+ * \author Andrej Leban
+ * \date 7/2019
+ * Ch. 8 Trees
+ */
+
+#include <iostream>
+#include <sstream>
+
+// common libraries
+#include "common/io.h"
+#include <common/numeric.h>
+
+#include "payoff2.h"
+#include "random.h"
+
+using namespace der;
+
+int main()
+{
+    return 0;
+}
+
